f23e4e67d7965d3939886ac7be62ecb75bc25a9e
Geniuska275 <aigbojie2020@gmail.com>
2024-09-16 17:34:55 +0100

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-
+import {motion} from "framer-motion"
 
 export const About = () => {
   return (
@@ -7,16 +7,26 @@ export const About = () => {
             <span className="text-neutral-500 ml-3">Me</span>
         </h1>
         <div className="flex flex-wrap">
-            <div className="w-full lg:w-1/2 lg:p-8">
+            <motion.div 
+            whileInView={{opacity:1,x:0}}
+            initial={{opacity:0,x:-100}}
+            transition={{duration:0.5}}
+            
+            className="w-full lg:w-1/2 lg:p-8">
               <div className="flex items-center justify-center">
                 <img className="w-[300px] rounded-2xl " src="https://images.unsplash.com/photo-1607294846590-fd005d7973e8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGV2ZWxvcGVyfGVufDB8fDB8fHww"/>
               </div>
-            </div>
-              <div className="w-full lg:w-1/2">
+            </motion.div>
+              <motion.div 
+               whileInView={{opacity:1,x:0}}
+               initial={{opacity:0,x:100}}
+               transition={{duration:0.5}}
+               
+              className="w-full lg:w-1/2">
                   <div className="flex justify-center lg:justify-start">
                     <p className="my-2 max-w-xl py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni est numquam assumenda temporibus? Magni quo totam, accusamus porro nostrum ex rem labore autem dolores ea qui vel natus nisi necessitatibus!</p>
                  </div>
-              </div>
+              </motion.div>
             <div>
 
             </div>
--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -6,38 +6,90 @@ import { TbBrandNextjs } from "react-icons/tb"
 import { TbBrandReactNative } from "react-icons/tb";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { SiTypescript } from "react-icons/si";
+import {motion} from "framer-motion"
 
-
+const iconVariants=(duration)=>({
+ initial:{y:-10},
+ animate:{
+   y:[10,-10],
+   transition:{
+      duration:duration,
+      ease:"linear",
+      repeat:Infinity,
+      repeatType:"reverse"
+   }
+ }
+})
 
 const Technology = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h2 className="my-20 text-center text-4xl">Technologies</h2>
+        <motion.h2
+        whileInView={{
+         opacity:1,
+         y:0
+        }}
+        initial={{
+         opacity:0,
+         y:-100
+        }} className="my-20 text-center text-4xl">Technologies</motion.h2>
         <div className="items-center flex flex-wrap justify-center gap-4">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <motion.div 
+            variants={iconVariants(2.4)}
+            initial="initial"
+            animate="animate"
+
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                <RiHtml5Line className="text-7xl text-orange-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+             variants={iconVariants(2.6)}
+             initial="initial"
+             animate="animate"
+
+            className="rounded-2xl border-4 border-neutral-800 p-4">
                <RiCss3Line className="text-7xl text-blue-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVariants(2.8)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                <RiTailwindCssFill  className="text-7xl text-cyan-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
                <RiJavascriptLine className="text-7xl text-yellow-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVariants(3.2)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                <SiTypescript className="text-7xl text-yellow-300"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVariants(3.4)}
+            initial="initial"
+            animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
                <RiReactjsLine className="text-7xl text-cyan-300"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVariants(3.6)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                <TbBrandNextjs className="text-7xl "/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVariants(3.8)}
+            initial="initial"
+            animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
                <TbBrandReactNative  className="text-7xl text-cyan-400"/>
-            </div>
+            </motion.div>
 
         </div>
 
