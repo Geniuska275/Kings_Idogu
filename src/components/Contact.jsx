2ba83ac1b3debe029d24c6a828e36cdb4f933aea
Geniuska275 <aigbojie2020@gmail.com>
2024-09-17 21:24:30 +0100

    updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-
+import {motion} from "framer-motion"
 const Contact = () => {
     const contact={
         address:"Lagos,Nigeria",
@@ -8,10 +8,27 @@ const Contact = () => {
     }
   return (
     <div className="border-b border-neutral-900 pb-20">
-        <h1 className="my-10 text-center text-4xl ">Get in Touch</h1>
+        <motion.h1 
+        whileInView={{opacity:1,y:0}}
+        initial={{opacity:0,y:-100}}
+        transition={{duration:0.5}}
+
+        className="my-10 text-center text-4xl ">Get in Touch</motion.h1>
         <div className="text-center tracking-tighter">
-            <p className="my-4 ">{contact.address}</p>
-            <p className="my-4 underline ">{contact.phone}</p>
+            <motion.p 
+            whileInView={{opacity:1,x:0}}
+            initial={{opacity:0,x:-100}}
+            transition={{duration:1}}
+
+            className="my-4 ">
+              {contact.address}</motion.p>
+            <motion.p 
+             whileInView={{opacity:1,x:0}}
+             initial={{opacity:0,x:100}}
+             transition={{duration:1}}
+            className="my-4 underline ">
+              {contact.phone}
+              </motion.p>
             <a href="mailto:aigbojie2020@gmail.com" className=" border-b ">{contact.email}</a>
 
         </div>
