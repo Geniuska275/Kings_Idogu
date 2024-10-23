f6f4bf32432d06156e82da1ec3893a364996a39d
Geniuska275 <aigbojie2020@gmail.com>
2024-10-23 11:50:15 +0100

    updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
        description:"An Information website for GreenFusion Energy ltd ",
        image:"https://images.unsplash.com/photo-1719937051230-8798ae2ebe86?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
        technologies:["Reactjs","Tailwindcss","Typescript"],
-        projectlink:"https://www.greenfusion.enery"
+        projectlink:"https://www.greenfusion.energy"
      },
  
     { 
