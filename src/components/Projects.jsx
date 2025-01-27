8f9f57687efc8f6ebeff4bcad51adcebc05cad1b
Geniuska275 <aigbojie2020@gmail.com>
2025-01-27 13:44:15 +0100

    project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -43,7 +43,7 @@ const Projects = () => {
       title:"CBTforge",
       description:"An Educational website ",
       image:"https://images.unsplash.com/photo-1719937051230-8798ae2ebe86?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
-      technologies:["Reactjs","Tailwindcss","Typescript"],
+      technologies:["Reactjs","Tailwindcss"],
       projectlink:"https://cbtforge.vercel.app/"
     },
    ]
